ecded0522359d4049abdee0db77bd50aa7ea3c14
Moceb <47651676+MoCeb@users.noreply.github.com>
2020-10-08 17:07:10 -0400

    Update kick.js
    Here is the updated version i didn't get to test it much so idk there might be some bugs

--- commands/kick.js
+++ commands/kick.js
@@ -4,12 +4,10 @@ module.exports = {
     name: 'kick',
     description: 'Can you read? [STAFF ONLY]',
     execute(message, args) {
-        const person = message.mentions.members.first();
-
-
+        const person = message.author.id 
 
         const kickEmbedWithoutPermissions = new Discord.MessageEmbed()
-            .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true, format: "png"}))
+            .setAuthor(message.author.id , message.author.displayAvatarURL({dynamic: true, format: "png"}))
             .setDescription(`Congrats. If you actually thought we would let you kick someone without required perms, you must be delusional. Nice try.`)
             .setFooter('You just earned yourself the muted role.')
 
@@ -22,7 +20,7 @@ module.exports = {
             message.channel.send('Oi. Can you like, actually ping someone?')
         } else {
             const kickEmbedWithPermissions = new Discord.MessageEmbed()
-                .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true, format: "png"}))
+                .setAuthor(message.author.id, message.author.displayAvatarURL({dynamic: true, format: "png"}))
                 .setDescription(`Kicked ${person}`)
 
             message.channel.send(
