91e63e28000b87aeadf907e1a6318999bb79d889
xxshadowslayer <54245752+xxshadowslayer@users.noreply.github.com>
2020-10-07 22:06:49 -0400

    Added 'docs' command.

---
+++ commands/docs.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "docs",
+    description: "This command is public, but you probably don't know how to use it.",
+    execute(message, args) {
+        const request = require('request')
+
+        const query = args[0].replace("#", ".")
+
+        request(`https://djsdocs.sorta.moe/v2/embed?src=stable&q=${query}`, { json: true }, (err, res, body) => {
+            message.channel.send("Result(s):", {embed: body})
+            console.log(body.toString() + "\n\n\n\n" + res.toString())
+        })
+    }
+}
